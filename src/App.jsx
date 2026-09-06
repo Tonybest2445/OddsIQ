e23757b9b75f43013484d2f2b80942415af56421
Tonybest2445 <131904505+Tonybest2445@users.noreply.github.com>
2026-09-06 19:39:32 +0100

    Wrap routes in AuthProvider for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './lib/AuthContext'
 import Home from './pages/Home'
 import BuildCombo from './pages/BuildCombo'
 import ViewCombo from './pages/ViewCombo'
+import Account from './pages/Account'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/build-combo" element={<BuildCombo />} />
-      <Route path="/combo/:code" element={<ViewCombo />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/build-combo" element={<BuildCombo />} />
+        <Route path="/combo/:code" element={<ViewCombo />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </AuthProvider>
   )
 }
 
