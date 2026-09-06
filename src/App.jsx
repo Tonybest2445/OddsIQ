10c1a48047739ba89792ee48a0f969821cf916f8
Tonybest2445 <131904505+Tonybest2445@users.noreply.github.com>
2026-09-06 12:31:53 +0100

    Add routes for BuildCombo and ViewCombo pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import BuildCombo from './pages/BuildCombo'
+import ViewCombo from './pages/ViewCombo'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/build-combo" element={<BuildCombo />} />
+      <Route path="/combo/:code" element={<ViewCombo />} />
     </Routes>
   )
 }
